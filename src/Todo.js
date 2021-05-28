5a0f2d0645933c5131d52907476ed6e1e0c710fd
Varun Khalate <khalatevarun@gmail.com>
2021-05-28 17:22:55 +0530

    add update task functionality

--- src/Todo.js
+++ src/Todo.js
@@ -5,23 +5,73 @@ import {
   ListItemAvatar,
   ListItemText,
   Button,
+  Modal,
+  makeStyles,
 } from '@material-ui/core';
 import React from 'react';
 import './Todo.css';
 import db from './firebase';
+import { useState } from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
 function Todo(props) {
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+  const classes = useStyles();
+
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState(props.task.todo);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const updateTodo = () => {
+    db.collection('todos').doc(props.task.id).set(
+      {
+        todo: input,
+      },
+      { merge: true },
+    );
+
+    setOpen(false);
+  };
+
   return (
-    <List className="todo_list">
-      <ListItem>
-        <ListItemAvatar></ListItemAvatar>
-        <ListItemText primary={props.task.todo} secondary="Dummy deadline..." />
-      </ListItem>
-      <Button
-        onClick={(event) => db.collection('todos').doc(props.task.id).delete()}
-      >
-        Delete
-      </Button>
-    </List>
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div className={classes.paper}>
+          <h1>open</h1>
+          <input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button onClick={updateTodo}>Update Todo</Button>
+        </div>
+      </Modal>
+      <List className="todo_list">
+        <ListItem>
+          <ListItemAvatar></ListItemAvatar>
+          <ListItemText
+            primary={props.task.todo}
+            secondary="Dummy deadline..."
+          />
+        </ListItem>
+        <button onClick={(e) => setOpen(true)}>Edit</button>
+        <DeleteIcon
+          onClick={(event) =>
+            db.collection('todos').doc(props.task.id).delete()
+          }
+        />
+      </List>
+    </>
   );
 }
 
